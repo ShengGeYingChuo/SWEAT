cf9beaebca571369798dc8d8916a864c761fa8b4
ShengGeYingChuo <144237250+ShengGeYingChuo@users.noreply.github.com>
2023-09-06 14:22:36 +0200

    Delete test directory

--- test/test/uptest.php
+++
@@ -1,8 +0,0 @@
-
-title:<?php echo $_POST["m_code"]; ?> <br>
-module name:<?php echo $_POST["module"]; ?> <br>
-Co-ordinator:<?php echo $_POST["co_ordinator"]; ?> <br>
-Semester:<?php echo $_POST["semester"]; ?> <br>
-Credits:<?php echo $_POST["credits"]; ?> <br>
-CA(%):<?php echo $_POST["CA"]; ?> <br>
-
